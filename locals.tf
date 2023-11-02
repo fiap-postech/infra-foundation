7a7ce7306c4ec7897abdda0ceec3a4f535141f35
Eberson Oliveira <eberson.olivera@gmail.com>
2023-11-02 14:33:28 -0300

    added third az subnet public and private

--- locals.tf
+++ locals.tf
@@ -14,17 +14,25 @@ locals {
       "Public Subnet B" : {
         cidr_block        = "172.30.2.0/24"
         availability_zone = "us-east-1b"
+      },
+      "Public Subnet C" : {
+        cidr_block        = "172.30.3.0/24"
+        availability_zone = "us-east-1c"
       }
     }
 
     private = {
       "Private Subnet A" : {
-        cidr_block        = "172.30.3.0/24"
+        cidr_block        = "172.30.4.0/24"
         availability_zone = "us-east-1a"
       },
       "Private Subnet B" : {
-        cidr_block        = "172.30.4.0/24"
+        cidr_block        = "172.30.5.0/24"
         availability_zone = "us-east-1b"
+      },
+      "Private Subnet B" : {
+        cidr_block        = "172.30.6.0/24"
+        availability_zone = "us-east-1c"
       }
     }
   }
